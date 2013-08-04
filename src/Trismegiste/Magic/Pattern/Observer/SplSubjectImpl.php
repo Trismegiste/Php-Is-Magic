6b00e507dc285f338f8bce9753c18d5612ff8fa8
Trismegiste <flo@trismegiste.net>
2013-08-04 03:49:50 +0200

    trait impl for subject

---
+++ src/Trismegiste/Magic/Pattern/Observer/SplSubjectImpl.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Are-Php-Traits-Good-Or-Evil
+ */
+
+namespace Trismegiste\Magic\Pattern\Observer;
+
+/**
+ * SplSubjectImpl is an standard implementation of a \SplSubject
+ */
+trait SplSubjectImpl
+{
+
+    private $observerList;
+
+    protected function getObserver()
+    {
+        if (is_null($this->observerList)) {
+            $this->observerList = new \SplObjectStorage();
+        }
+
+        return $this->observerList;
+    }
+
+    public function attach(\SplObserver $observer)
+    {
+        $this->getObserver()->attach($observer);
+    }
+
+    public function detach(\SplObserver $observer)
+    {
+        $this->getObserver()->detach($observer);
+    }
+
+    public function notify()
+    {
+        foreach ($this->getObserver() as $observer) {
+            $observer->update($this);
+        }
+    }
+
+}
---
+++ tests/Trismegiste/Magic/Pattern/Observer/SplSubjectImplTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Php is magic
+ */
+
+namespace tests\Trismegiste\Magic\Pattern\Observer;
+
+use Trismegiste\Magic\Pattern\Observer\SplSubjectImpl;
+
+/**
+ * SplSubjectImplTest tests the SplSubjectImpl trait
+ */
+class SplSubjectImplTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $subject;
+    protected $observer;
+
+    protected function setUp()
+    {
+        $this->observer = $this->getMock('SplObserver');
+        $this->subject = new SubjectExample();
+    }
+
+    public function testSubscribe()
+    {
+        $this->subject->attach($this->observer);
+        $this->observer
+                ->expects($this->once())
+                ->method('update')
+                ->with($this->subject);
+        // notify observer
+        $this->subject->notify();
+    }
+
+}
+
+// example test
+class SubjectExample implements \SplSubject
+{
+
+    use SplSubjectImpl;
+}
+
