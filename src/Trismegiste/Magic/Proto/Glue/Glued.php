80e0187240f2f34553277b5e27a3a815bafebb24
Trismegiste <flo@trismegiste.net>
2013-09-11 01:04:28 +0200

    change scope binding

--- src/Trismegiste/Magic/Proto/Glue/Glued.php
+++ src/Trismegiste/Magic/Proto/Glue/Glued.php
@@ -7,25 +7,35 @@
 namespace Trismegiste\Magic\Proto\Glue;
 
 /**
- * Glued is a aggregation of methods javascript-like capability
+ * Glued is an aggregation of methods javascript-like capability.
+ * 
+ * Note : for experimental use only !
  */
 trait Glued
 {
 
     private $method = array();
 
-    public function __set($name, \Closure $func)
+    public function __set($name, $func)
     {
-        $this->method[$name] = \Closure::bind($func, $this, __CLASS__);
+        if ($func instanceof \Closure) {
+            // I could use __CLASS__ instead of get_called_class() but
+            // that means that private properties could be accessed from subclasses
+            // of a class which uses this trait. It's a design choice.
+            $this->method[$name] = \Closure::bind($func, $this, get_called_class());
+        } else {
+            // simple injection of public data
+            $this->$name = $func;
+        }
     }
 
     public function __call($name, $arguments)
     {
-        if (array_key_exists($name, $this->method)) {
-            return call_user_func_array($this->method[$name], $arguments);
-        } else {
+        if (!array_key_exists($name, $this->method)) {
             trigger_error('Call to undefined method ' . get_called_class() . "::$name", E_USER_ERROR);
         }
+
+        return call_user_func_array($this->method[$name], $arguments);
     }
 
 }
--- tests/Trismegiste/Magic/Proto/Glue/GluedTest.php
+++ tests/Trismegiste/Magic/Proto/Glue/GluedTest.php
@@ -7,7 +7,7 @@
 namespace tests\Trismegiste\Magic\Proto\Glue;
 
 /**
- * GluedTest tests Glued
+ * GluedTest tests Glued trait
  */
 class GluedTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,11 +15,11 @@ class GluedTest extends \PHPUnit_Framework_TestCase
     public function testPrivateAccessInClosure()
     {
         $obj = new GlueExample(21);
-        $obj->extract = function($mul) {
+        $obj->getAnswer = function($mul) {
                     return $mul * $this->property;
                 };
 
-        $this->assertEquals(42, $obj->extract(2));
+        $this->assertEquals(42, $obj->getAnswer(2));
     }
 
     /**
@@ -34,12 +34,23 @@ class GluedTest extends \PHPUnit_Framework_TestCase
 
     public function testPrivateAccessInSubclass()
     {
-        $obj = new GlueSubclassed(21);
-        $obj->extract = function($mul) {
-                    return $mul * $this->property;
+        $obj = new GlueSubclassed(666);
+        $obj->extract = function($pu) {
+                    // it's a choice to limit the scope of the bound closure
+                    // to the dynamic type, like a standard method would do
+                    // if declared in GlueSubclassed, regardless where
+                    // the trait is used
+                    $pu->assertFalse(property_exists($this, 'property'));
                 };
 
-        $this->assertEquals(42, $obj->extract(2));
+        $obj->extract($this);
+    }
+
+    public function testSimplePropInjection()
+    {
+        $obj = new GlueExample(33);
+        $obj->someData = 42;
+        $this->assertEquals(42, $obj->someData);
     }
 
 }
